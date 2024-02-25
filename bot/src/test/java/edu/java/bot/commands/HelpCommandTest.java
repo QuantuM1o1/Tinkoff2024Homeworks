54b65b926f437454c7464c8fa4850ebbc74dab10
Mikhail Vorobev <mishavorobiev@mail.ru>
2024-02-25 17:01:53 +0600

    Optimized imports

--- bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
+++ bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import groovy.transform.AutoImplement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
--- bot/src/test/java/edu/java/bot/commands/TrackCommandTest.java
+++ bot/src/test/java/edu/java/bot/commands/TrackCommandTest.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import kotlin.random.URandomKt;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
