e7a9466c75adb7c3d41c381bea7fbba8523f6b0f
Mikhail Vorobev <mishavorobiev@mail.ru>
2024-04-29 23:41:29 +0500

    Added bucket4j spring boot

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -7,10 +7,12 @@ import edu.java.configuration.StackOverflowClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties({
     ApplicationConfig.class,
     ResourcesConfig.class,
